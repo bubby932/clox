ff98bcca585d231b2deb2e46832ba3ed0c183fe9
Raven <82341739+bubby932@users.noreply.github.com>
2022-10-03 16:15:43 -0700

    wip

--- debug.c
+++ debug.c
@@ -37,10 +37,12 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 
     uint8_t instruction = chunk->code[offset];
     switch(instruction) {
-        case OP_RETURN:
-            return simpleInstruction("OP_RETURN", offset);
         case OP_NEGATE:
             return simpleInstruction("OP_NEGATE", offset);
+        case OP_PRINT:
+            return simpleInstruction("OP_PRINT", offset);
+        case OP_RETURN:
+            return simpleInstruction("OP_RETURN", offset);
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
         case OP_NIL:
@@ -48,7 +50,14 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_TRUE:
             return simpleInstruction("OP_TRUE", offset);
         case OP_FALSE:
-            return simpleInstruction("OP_FALSE", offset); 
+            return simpleInstruction("OP_FALSE", offset);
+        case OP_POP:
+            return simpleInstruction("OP_POP", offset);
+        case OP_GET_GLOBAL:
+            return constantInstruction("OP_GET_GLOBAL", chunk, offset);
+        case OP_DEFINE_GLOBAL:
+            return constantInstruction("OP_DEFINE_GLOBAL", chunk,
+                                       offset);
         case OP_EQUAL:
             return simpleInstruction("OP_EQUAL", offset);
         case OP_GREATER:
